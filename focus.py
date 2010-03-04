85a9f73531d3a5635755fe726c7e7a99a6af0592
Brad Jasper <bjasper@gmail.com>
2010-03-03 21:13:47 -0600

    got HostsFile object adding and writing new hosts

--- focus.py
+++ focus.py
@@ -1,68 +1,18 @@
 #!/usr/bin/env python
 
+from utils import get_focusfile, get_file_lines
+from base import HostsFile
 # Focus is a script that will block websites
 
-import os
-import logging
-import re
+hostsfile = HostsFile("/etc/hosts")
 
-hostsfile = "/etc/hosts"
-hostsfilebak = "/tmp/hosts.bak"
-dotfile = os.path.expanduser("~/.focus")
+for host in get_file_lines(get_focusfile()):
+    print host
+    hostsfile.add_host(host, "127.0.0.1")
 
-def get_file_lines(filename):
-    "Return all lines from a file"
+hostsfile.write("/etc/hosts", backup="/tmp/hosts.bak")
 
-    with open(filename) as file:
-        return (line.strip() for line in file.readlines())
-
-def split_hosts_line(line):
-    "Split a /etc/hosts line into manageable parts"
-
-    if line.startswith("#"):
-        return line
-
-    return re.split("\s+", line)
-
-def combine_hosts_line(line):
-    "Combine an /etc/hosts line back into the proper format"
-
-    if isinstance(line, basestring) and line.startswith("#"):
-        return line
-
-    return "\t".join(line)
-
-def host_exists(host, lines):
-    "Check if a host exists within an array of /etc/hosts lines"
-
-    for line in lines:
-        
-        if isinstance(line, list):
-            if line[1] == host:
-                return True
-    return False
-
-
-def add_new_hosts(lines, hosts):
-    "Add new hosts to a list if they don't already exist"
-
-    for host in hosts:
-        if host_exists(host, lines):
-            logging.warning("'%s' already exists in %s, skipping", host, hostsfile)
-        else:
-            lines.append(["127.0.0.1", host])
-    return lines
-
-
-def write_new_hosts(hosts):
-    "Write out the hosts to the /etc/hosts file"
-
-    print hostsfile, hostsfilebak
-    os.rename(hostsfile, hostsfilebak)
-
-    with open(hostsfile, "w") as file:
-        for host in hosts:
-            file.write(combine_hosts_line(host) + "\n")
+"""
 
 
 focus_urls = get_file_lines(dotfile)
@@ -73,3 +23,4 @@ new_lines = add_new_hosts(old_lines, focus_urls)
 write_new_hosts(new_lines)
 
 print "Done!"
+"""
