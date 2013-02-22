a473222d7639f6fdbc4fe670c6019a4a57efb71c
Brad Jasper <bjasper@gmail.com>
2013-02-21 23:10:47 -0500

    cleanup

--- focus.sh
+++ focus.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #
 # Focus
 #
@@ -7,28 +6,32 @@
 # under the host name 127.0.0.1 so you can't visit them....so you focus :)
 #
 
-focus_hosts() {
-    cat $HOME/.focus | while read host; do
-        echo -n " $host www.$host";
-    done
+focus() {
+    backup_hosts_file && add_hosts_line && echo "Focusing...go be productive!"
 }
 
-hosts_line() {
-    echo "127.0.0.1 $(focus_hosts) focus_activation_host"
+unfocus() {
+    backup_hosts_file && delete_hosts_line && echo "Unfocusing..were you productive?"
 }
 
 backup_hosts_file() {
     sudo cp /etc/hosts /etc/hosts.bak
 }
 
-focus() {
-    backup_hosts_file \
-        && sudo -s "echo $(hosts_line) >> /etc/hosts" \
-        && echo "Focusing...go be productive!"
+add_hosts_line() {
+    sudo -s "echo $(hosts_line) >> /etc/hosts"
 }
 
-unfocus() {
-    backup_hosts_file \
-        && sudo -s "sed '/focus_activation_host/d' /etc/hosts.bak > /etc/hosts" \
-        && echo "Unfocusing..were you productive?"
+delete_hosts_line() {
+    sudo -s "sed '/focus_activation_host/d' /etc/hosts.bak > /etc/hosts"
+}
+
+hosts_line() {
+    echo "127.0.0.1 $(focus_hosts) focus_activation_host"
+}
+
+focus_hosts() {
+    cat $HOME/.focus | while read host; do
+        echo -n " $host www.$host";
+    done
 }
